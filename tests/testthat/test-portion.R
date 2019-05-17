9ba5082406ca6917c22e5d21db4af6fd8e311c84
jkennel <jkennel@uoguelph.ca>
2019-05-16 22:13:36 -0400

    fix test

--- tests/testthat/test-portion.R
+++ tests/testthat/test-portion.R
@@ -2,7 +2,8 @@ test_that("portion works", {
   library(recipes)
 
   df <- tibble(x = 1, t = as.POSIXct(1:10, origin = '1970-01-01'))
-
+  freqs <- c(1,2)
+  
   # lags numeric data
   expect_error({recipe(~ ., data = df) %>%
     step_harmonic(t, freq = freqs) %>%
