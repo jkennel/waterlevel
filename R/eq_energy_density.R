7460f93e35bd76b4d8c97cdacc11b6eecf4e3095
jkennel <jkennel@uoguelph.ca>
2019-10-10 12:55:41 -0400

    energy density wang and manga

--- R/eq_energy_density.R
+++ R/eq_energy_density.R
@@ -1,5 +1,5 @@
 
-#' energy_density_tsuboi
+#' energy_density_wang
 #'
 #' @param magnitude Richter magnitude 
 #' @param distance epicentral distance
@@ -8,28 +8,10 @@
 #' @export
 #'
 #' @examples
-#' energy_density_tsuboi(5, 1000)
-energy_density_tsuboi <- function(magnitude, distance) {
-  # https://en.wikipedia.org/wiki/Richter_magnitude_scale
-  # Tsuboi, University of Tokyo empirical
-  10^(magnitude + 0.83 - 1.73 * log10(distance))
-}
-
-
-#' energy_density_lillie
-#'
-#' @param magnitude Richter magnitude 
-#' @param distance epicentral distance
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' energy_density_lillie(5, 1000)
-energy_density_lillie <- function(magnitude, distance) {
-  # https://en.wikipedia.org/wiki/Richter_magnitude_scale
-  # Lillie empirical
-  10^(magnitude + 2.48 - 2.76 * log10(distance))
+#' energy_density_wang(5, 9)
+energy_density_wang <- function(magnitude, distance) {
+  # Wang & Manga 2010
+  10^((log10(distance) + 1.4 - (0.48 * magnitude)) / -0.33)
 }
 
 
